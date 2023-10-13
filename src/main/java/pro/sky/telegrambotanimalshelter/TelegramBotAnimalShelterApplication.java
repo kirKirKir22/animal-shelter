a0b4d1811de57f3c4660b9ff2c56d84e25f83469
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2023-10-13 16:47:12 +0300

    первый коммит в ветке мастер. спринг запускается

---
+++ src/main/java/pro/sky/telegrambotanimalshelter/TelegramBotAnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambotanimalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotAnimalShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotAnimalShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/telegrambotanimalshelter/TelegramBotAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambotanimalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotAnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
