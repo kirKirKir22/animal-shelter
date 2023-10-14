f1140614425c13433fed29fdcf9f3ecf49dbcd04
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2023-10-14 16:51:43 +0300

    бот реагирует на команду "/start" в телеграм

--- src/main/java/pro/sky/telegrambotanimalshelter/TelegramBotAnimalShelterApplication.java
+++ src/main/java/pro/sky/telegrambotanimalshelter/TelegramBotAnimalShelterApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelegramBotAnimalShelterApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotAnimalShelterApplication.class, args);
 	}
